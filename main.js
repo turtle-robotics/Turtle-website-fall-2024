3ee0680f03aeb0bee10b57ad2e1217ba28564a90
AbdulaiSeydi <abdulaisb@tamu.edu>
2024-07-14 06:09:00 -0500

    hamburger menu

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ var w = window.innerWidth;
 var h = window.innerHeight;
 var x = document.getElementById("data");
 x.innerHTML = "Browser width: " + w + ", height: " + h + ".";
+
+const burger = document.querySelector(".hamburger");
+const burgerLinks = document.querySelector(".burger-links");
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    burgerLinks.classList.toggle("active");
+
+})
