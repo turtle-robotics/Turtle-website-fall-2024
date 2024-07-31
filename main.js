c66d3805dadc7a75e72ed92c51ad14ae86b66c61
AbdulaiSeydi <abdulaisb@tamu.edu>
2024-07-31 18:34:27 -0500

    burger menu finished

--- main.js
+++ main.js
@@ -6,8 +6,17 @@ x.innerHTML = "Browser width: " + w + ", height: " + h + ".";
 const burger = document.querySelector(".hamburger");
 const burgerLinks = document.querySelector(".burger-links");
 
+
+
 burger.addEventListener("click", () => {
+    const visibility = burgerLinks.getAttribute('data-visible');
     burger.classList.toggle("active");
     burgerLinks.classList.toggle("active");
+        if (visibility === "true") {
+            burgerLinks.setAttribute('data-visible', "false");
+        }
+        else {
+            burgerLinks.setAttribute('data-visible', "true");
+        }
 
 })
